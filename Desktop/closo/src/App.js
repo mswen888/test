9e5e0aade89ac484394227dbd38ea04a6644c387
mswen888 <bobapapershop@gmail.com>
2021-11-07 10:33:49 -0700

    new

--- Desktop/closo/src/App.js
+++ Desktop/closo/src/App.js
@@ -1,141 +1,141 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/splide.min.css";
-import React, { useState, useRef, useEffect, useLayoutEffect } from "react";
+
 import "./index.css";
 
-function ManualCarousel({ offset }) {
-  return (
-    <div className="App">
-      <Splide
-        options={{
-          type: "slide",
-          rewind: "false",
-          perPage: 3,
-          perMove: 3,
-          gap: "1rem",
-          pagination: false,
-          padding: `${offset}rem`,
-          breakpoints: {
-              640: {
-                type: 'loop',
-                perMove: 1,
-                padding: '2rem',
-                perPage: 1,
-              }
-          },
-        }}
-        renderControls={() => (
-          <div className="absolute flex w-4 h-4 space-x-6 splide__arrows -top-8 right-12 invisible sm:visible">
-            <div className="splide__arrow-prev" role="button">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M11 17l-5-5m0 0l5-5m-5 5h12"
-                />
-              </svg>
-            </div>
-            <div className="splide__arrow--next" role="button">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M13 7l5 5m0 0l-5 5m5-5H6"
-                />
-              </svg>
-            </div>
-          </div>
-        )}
-      >
-        <SplideSlide>
-          <div className="bg-red-300">one</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className="bg-red-300">two</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className="bg-red-300">three</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className="bg-red-300">four</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className="bg-red-300">five</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className="bg-red-300">six</div>
-        </SplideSlide>
-      </Splide>
-    </div>
-  );
-}
-
 function App() {
-  const screenSize = useRef(null);
-  const elementRef = useRef(null);
-
-  const [marginLeft, setMarginLeft] = useState(0);
-  const [paddingLeft, setPaddingLeft] = useState(0);
-
-  // const retrieveMargin = (elm) => {
-  //   if (elm != null) {
-  //     let styles = window.getComputedStyle(elm);
-  //     let ml = styles.getPropertyValue("margin-left");
-  //     setMarginLeft(Number.parseInt(ml.replace("px", "")));
-
-  //     let pl = styles.getPropertyValue("padding-left");
-  //     setPaddingLeft(Number.parseInt(pl.replace("px", "")));
-  //   }
-  // }
-  const retrieveMargin = () => {
-    const styles = window.getComputedStyle(elementRef.current);
-    const ml = styles.getPropertyValue("margin-left");
-    let pl = styles.getPropertyValue("padding-left");
-    setPaddingLeft(Number.parseInt(pl.replace("px", "")));
-    setMarginLeft(Number.parseInt(ml.replace("px", "")));
-  };
-
-  useLayoutEffect(() => {
-    window.addEventListener("resize", () => {
-      retrieveMargin();
-    });
-    return () => {
-      window.removeEventListener("resize", () => {
-        retrieveMargin();
-      });
-    };
-  }, []);
-
-  const offset = (paddingLeft + marginLeft) / 16;
-
-  console.log("OFFSET", offset);
+  const perPage = 3;
+  const [isFirst, setIsFirst] = useState(true);
+  const [isLast, setIsLast] = useState(false);
 
   return (
-    <div className=" p-4 pt-6">
-      <h2
-        className="max-w-6xl mx-auto px-4 s-top-8 pb-4 text-lg font-bold"
-        ref={elementRef}
-      >
+    <div className="max-w-6xl mx-auto p-4 pt-6">
+      <h2 className="-top-8 pb-4 text-lg font-bold ml-5 sm:ml-0">
         This is the title
       </h2>
-
-      <ManualCarousel offset={offset} />
+      <div className="App">
+        <Splide
+          options={{
+            type: "slide",
+            rewind: "false",
+            perPage: perPage,
+            perMove: 3,
+            gap: "1rem",
+            pagination: false,
+          }}
+          renderControls={() => (
+            <div className="absolute flex w-4 h-4 space-x-6 splide__arrows -top-8 right-12 invisible sm:visible">
+              <div
+                className={`splide__arrow-prev ${
+                  isFirst ? "text-red-500" : ""
+                }`}
+                role="button"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M11 17l-5-5m0 0l5-5m-5 5h12"
+                  />
+                </svg>
+              </div>
+              <div
+                className={`splide__arrow--next ${
+                  isLast ? "text-red-500" : ""
+                }`}
+                role="button"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M13 7l5 5m0 0l-5 5m5-5H6"
+                  />
+                </svg>
+              </div>
+            </div>
+          )}
+          onMoved={(splide, newIndex) => {
+            setIsLast(newIndex >= splide.length - perPage);
+            setIsFirst(newIndex === 0);
+          }}
+        >
+          <SplideSlide>
+            <img
+              src="https://www.istockphoto.com/resources/images/PhotoFTLP/1035146258.jpg"
+              alt="test image"
+            />
+            <div className="bg-red-300">one</div>
+          </SplideSlide>
+          <SplideSlide>
+            <img
+              src="https://www.istockphoto.com/resources/images/PhotoFTLP/1035146258.jpg"
+              alt="test image"
+            />
+            <div className="bg-red-300">
+              two two longer test two two two two two two very long test
+            </div>
+          </SplideSlide>
+          <SplideSlide>
+            <img
+              src="https://www.istockphoto.com/resources/images/PhotoFTLP/1035146258.jpg"
+              alt="test image"
+            />
+            <div className="bg-red-300">three</div>
+          </SplideSlide>
+          <SplideSlide>
+            <img
+              src="https://www.istockphoto.com/resources/images/PhotoFTLP/1035146258.jpg"
+              alt="test image"
+            />
+            <div className="bg-red-300">four</div>
+          </SplideSlide>
+          <SplideSlide>
+            <img
+              src="https://www.istockphoto.com/resources/images/PhotoFTLP/1035146258.jpg"
+              alt="test image"
+            />
+            <div className="bg-red-300">five</div>
+          </SplideSlide>
+          <SplideSlide>
+            <img
+              src="https://www.istockphoto.com/resources/images/PhotoFTLP/1035146258.jpg"
+              alt="test image"
+            />
+            <div className="bg-red-300">six</div>
+          </SplideSlide>
+          <SplideSlide>
+            <img
+              src="https://www.istockphoto.com/resources/images/PhotoFTLP/1035146258.jpg"
+              alt="test image"
+            />
+            <div className="bg-red-300">seven</div>
+          </SplideSlide>
+          <SplideSlide>
+            <img
+              src="https://www.istockphoto.com/resources/images/PhotoFTLP/1035146258.jpg"
+              alt="test image"
+            />
+            <div className="bg-red-300">eight</div>
+          </SplideSlide>
+        </Splide>
+      </div>
     </div>
   );
 }
